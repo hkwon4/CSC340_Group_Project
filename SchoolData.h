a8c95e00a029879158318dcd232d6b8c5927c9c1
Hyok In Kwon <99067273+hkwon4@users.noreply.github.com>
2022-04-19 11:58:45 -0700

    Update SchoolData.h
    added comments and edited variable names to be more cohesive

--- SchoolData.h
+++ SchoolData.h
@@ -1,5 +1,5 @@
 //
-//  DataVariables.h
+//  SchoolData.h
 //  
 //  collection of data from: https://www.kaggle.com/datasets/wsj/college-salaries?select=degrees-that-pay-back.csv
 //  and: https://www.kaggle.com/datasets/wsj/college-salaries?select=salaries-by-region.csv
@@ -8,18 +8,21 @@
 //  declarations of class:
 //                          -Degrees
 //                          -SchoolRegion
+//
+//created by: Hyok In Kwon
 
 #ifndef SchoolData_h
 #define SchoolData_h
 #include <string>
 
 namespace NS_SCHOOLDATA{
+    using namespace std;
 /**
  * relevant data:
  * 
  * for Degrees class:
  * 
- * Major  : unique data of major
+ * major  : unique data of major
  * mStart : starting median salary
  * mMed   : mid-career median salary
  * percentChange: percent change from starting to mid-career salary
@@ -33,31 +36,33 @@ class Degree{
 public:
 
     Degree(string major,  double mStart,  double mMed,  double percent,  double mC10,  double mc25,  double mc75,  double mc90);
-    string getMajor() const;
-     double getStart() const;
-     double getMed() const;
-     double getPercent() const;
-     double getMid10() const;
-     double getMid25() const;
-     double getMid75() const;
-     double getMid90() const;
-
-     void setMajor(string newM);
-     void setStart(double newS);
-     void setMed(double newM);
-     void setMid10(double newM);
-     void setMid25(double newM);
-     void setMid75(double newM);
-     void setMid90(double newM);
+    
+    //accessors
+    string getMajor() const;   //return major
+    double getStart() const;   //return starting median salary
+    double getMed() const;     //return mid-career median salary
+    double getPercent() const; //return percent change
+    double getMid10() const;   //return mid-career 10th percentile salary
+    double getMid25() const;   //return mid-career 25th percentile salary
+    double getMid75() const;   //return mid-career 75th percentile salary
+    double getMid90() const;   //return mid-career 90th percentile salary
+    //mutators
+    void setMajor(string newM);     //newM => major
+    void setStart(double newS);     //newS => mStart
+    void setMed(double newM);       //newM => mMed
+    void setMid10(double newM);     //newM => mid10
+    void setMid25(double newM);     //newM => mid25
+    void setMid75(double newM);     //newM => mid75
+    void setMid90(double newM);     //newM => mid90
 private:
     string major;
-     double mStart;
-     double mMed;
-     double percentChange;
-     double mid10;
-     double mid25;
-     double mid75;
-     double mid90;
+    double mStart; // in dollars (output as fixed, setprecision(2))
+    double mMed;   // in dollars
+    double percentChange; // percentage
+    double mid10;  // in dollars
+    double mid25;  // in dollars
+    double mid75;  // in dollars
+    double mid90;  // in dollars
 }
 /*
 * for SchoolRegion class:
@@ -76,25 +81,26 @@ class SchoolRegion{
 
 public:
 
-    SchoolRegion(string name, string region, double mStart, double mMed, double mid10, double mid25, double mid75, double mid90);
+    SchoolRegion(string name, string region); //constructor with only name values as there are some missing values in some data 
 
-    string getName() const;
-    string getRegion() const;
-    double getStart() const;
-    double getMed() const;
-    double getMid10() const;
-    double getMid25() const;
-    double getMid75() const;
-    double getMid90() const;
-
-    void setName(string newN);
-    void setRegion(string newR);
-    void setStart(double newS);
-    void setMed(double newM);
-    void setMid10(double newM);
-    void setMid25(double newM);
-    void setMid75(double newM);
-    void setMid90(double newM);
+    //accessors
+    string getName() const;     //return name
+    string getRegion() const;   //return region
+    double getStart() const;    //return mStart
+    double getMed() const;      //return mMed
+    double getMid10() const;    //return mid10
+    double getMid25() const;    //return mid25
+    double getMid75() const;    //return mid75
+    double getMid90() const;    //return mid90
+    //mutators
+    void setName(string newN);      //newN => name
+    void setRegion(string newR);    //newR => region
+    void setStart(double newS);     //newS => mStart
+    void setMed(double newM);       //newM => mMed
+    void setMid10(double newM);     //newM => mid10
+    void setMid25(double newM);     //newM => mid25
+    void setMid75(double newM);     //newM => mid75
+    void setMid90(double newM);     //newM => mid90
 
 private:
 
