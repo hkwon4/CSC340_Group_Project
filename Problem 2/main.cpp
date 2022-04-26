2341507255610102eaee5aea4d930b2fdb02f085
Dvalera22 <99306238+Dvalera22@users.noreply.github.com>
2022-04-25 19:19:43 -0700

    Update main.cpp

--- Problem 2/main.cpp
+++ Problem 2/main.cpp
@@ -1,63 +1,77 @@
 //Created by Dexter and Antonio
+
+#include "SchoolData.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <utility>
+ 
+
+using namespace std;
+
 //function to get the average
-double getAverage(int count, double total) {
+double getAverage(int count, double total){
     double avg = 0;
-
+    
+    
     avg = (total / count);
-
+    
     return avg;
 }
 
-/*pair<string, double>*/ double paired(vector<string> RegionList, vector<double> salaryList) {
-    pair<string, double> pairValues;
-    int countCA = 0;
+vector<pair<string, double>> paired(vector<string> RegionList, vector<double> salaryList){
+    vector<pair<string, double>> pairValues;
+    int countCA=0;
     int countWest = 0;
     int countMidWest = 0;
     int countSouth = 0;
     int countNorthEast = 0;
-
-    double salaryCA = 0;
+    
+    double salaryCA=0;
     double salaryWest = 0;
     double salaryMidWest = 0;
     double salarySouth = 0;
-    int salaryNorthEast = 0;
-
-
+    double salaryNorthEast = 0;
+    
+    double averageCA=0;
+    double averageWest = 0;
+    double averageMidWest = 0;
+    double averageSouth = 0;
+    double averageNorthEast = 0;
+    
     string Region;
     int regionVal = 0;
-    for (int i = 0; i < RegionList.size(); i++) {
+    for(int i = 0; i < RegionList.size(); i++){
         Region = RegionList.at(i);
 
-        if (Region == "California") {
-            regionVal = 1;
+        if(Region == "California"){
+        regionVal = 1;
             salaryCA += salaryList.at(i);
-        }
-        else if (Region == "Western") {
-            regionVal = 2;
-            salaryWest += salaryList.at(i);
+    }else if(Region == "Western"){
+        regionVal = 2;
+        salaryWest += salaryList.at(i);
 
-        }
-        else if (Region == "Midwestern") {
-            regionVal = 3;
-            salaryMidWest += salaryList.at(i);
+    }else if(Region == "Midwestern"){
+        regionVal = 3;
+        salaryMidWest += salaryList.at(i);
 
-        }
-        else if (Region == "Southern") {
-            regionVal = 4;
-            salarySouth += salaryList.at(i);
+    }else if(Region == "Southern"){
+        regionVal = 4;
+        salarySouth += salaryList.at(i);
 
-        }
-        else if (Region == "Northeastern") {
-            regionVal = 5;
-            salaryNorthEast += salaryList.at(i);
+    }else if(Region == "Northeastern"){
+        regionVal = 5;
+        salaryNorthEast += salaryList.at(i);
 
-        }
-
-
-        switch (regionVal) {
+    }
+    
+    
+    switch (regionVal) {
         case 1:
             countCA++;
-
+            
             break;
         case 2:
             countWest++;
@@ -73,21 +87,25 @@ double getAverage(int count, double total) {
             break;
         default:
             break;
-        }
-
     }
-
-    double average = getAverage(countCA, salaryCA);
-
-    return average;
-
+        
+    }
+    
+    // for CA
+     averageCA = getAverage(countCA, salaryCA);
+     pairValues.push_back(make_pair("California", averageCA));
+   // for Western
+    averageWest = getAverage(countWest, salaryWest);
+    pairValues.push_back(make_pair("Western", averageWest));
+   //midWest
+    averageMidWest = getAverage(countMidWest, salaryMidWest);
+    pairValues.push_back(make_pair("MidWest", averageMidWest));
+  //South
+    averageSouth = getAverage(countSouth, salarySouth);
+    pairValues.push_back(make_pair("Southern", averageSouth));
+    //NorthEast
+    averageNorthEast = getAverage(countNorthEast, salaryNorthEast);
+    pairValues.push_back(make_pair("NorthEastern", averageNorthEast));
+    return pairValues;
+    
 }
-
-int main()
-{
-    vector<string> regions = { "California", "California", "Western", "Southern", "Northeastern" };
-    vector<double> values = { 100.00, 250.50,300.45,45.73,50.50 };
-    cout << paired(regions, values) << endl;
-    return 0;
-}
-
